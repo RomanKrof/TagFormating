e82deca8b8998089065013c24a261421997bb4cf
Roman Krof <krof.roman.2018@skola.ssps.cz>
2021-11-01 21:20:52 +0100

    Přidejte soubory projektu.

---
+++ TagFormating/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace TagFormating
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(@"Tagy:
+Capital <cap>
+Exclamation <exc>
+Dot <dot>
+Underscore <und>
+Bracket <bra>
+Ukončení tagu: </...>
+Zadaný text s tagy: ");          
+            string text = Console.ReadLine();
+
+            Tags Capital = new Tags("cap");
+            Tags Exclamation = new Tags("exc");
+            Tags Dot = new Tags("dot");
+            Tags Underscore = new Tags("und");
+            Tags Bracket = new Tags("bra");
+
+            foreach (Tags tagger in Tags.TagList)
+            {
+                int i = text.IndexOf(tagger.Name);
+
+                if (i == -1)
+                {
+                    continue;
+                }
+
+                if (text.Substring(i - 1, 5) == '<' + tagger.Name + '>')
+                {
+                    int j = text.IndexOf(tagger.Name, i+1);
+                    if (j == -1)
+                    {
+                        continue;
+                    }
+
+                    if (text.Substring(j - 2, 6) == "</" + tagger.Name + '>')
+                    {
+                        string tagText = Tags.Formatting(text.Substring(i + 4, j - i - 6), tagger.Name);
+
+                        text = text.Substring(0, i - 1) + tagText + text.Substring(j + 4);
+                    }
+                }
+            }
+            Console.WriteLine(text);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TagFormating/Tags.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TagFormating
+{
+    class Tags
+    {
+        public static List<Tags> TagList = new List<Tags>();
+
+        public Tags(string n)
+        {
+            Name = n;
+            TagList.Add(this);
+        }
+
+        public static string Formatting(string text, string tag)
+        {
+            string tagText = "";
+            //Tags Capital, Exclamation, Dot, Underscore, Bracket
+            if (tag == "cap")
+            {
+                tagText = text.ToUpper();
+                return tagText;
+            }
+
+            if (tag == "exc")
+            {
+                tagText = "!" + text + "!";
+                return tagText;
+            }
+
+            if (tag == "dot")
+            {
+                tagText = "." + text + ".";
+                return tagText;
+            }
+
+            if (tag == "und")
+            {
+                tagText = "_" + text + "_";
+                return tagText;
+            }
+
+            if (tag == "bra")
+            {
+                tagText = "(" + text + ")";
+                return tagText;
+            }
+
+            else
+            {
+                return tagText;
+            }
+        }
+
+        public string Name
+        {
+            get; private set;
+        }        
+    }
+}
